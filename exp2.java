c3304df519655c4658526df12235d46ab6bd4bb0
Sun Kaiyan <91142574+SZU999@users.noreply.github.com>
2023-12-12 15:59:18 +0800

    exp2.java

--- exp2.java
+++ exp2.java
@@ -1,37 +1,40 @@
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.nextLine();
-        LinkedList<Summon> list1 = new LinkedList<>();//先手玩家的英雄和随从，位置0为英雄，位置1--7为随从
-        LinkedList<Summon> list2 = new LinkedList<>();//后手玩家的英雄和随从，位置0为英雄，位置1--7为随从
-        LinkedList<Summon> currentPlayer = list1;//当前玩家
-        LinkedList<Summon> opponent = list2;//对手玩家
-        list1.add(new Summon(0, 30));//先初始化英雄，攻击力为0，生命值为30
+        LinkedList<Summon> list1 = new LinkedList<>();
+        LinkedList<Summon> list2 = new LinkedList<>();
+        LinkedList<Summon> currentPlayer = list1;
+        LinkedList<Summon> opponent = list2;
+        list1.add(new Summon(0, 30));
         list2.add(new Summon(0, 30));
         for (int i = 0; i < n; i++) {
             String tmp = sc.nextLine();
-            String arr[] = tmp.split("\\ ");
-            if (arr[0].equals("summon")) {
-                //在arr[1]的位置插入一个随从
+            String[] arr = tmp.split("\\ ");
+            if ("summon".equals(arr[0])) {
                 currentPlayer.add(Integer.parseInt(arr[1]),
                         new Summon(Integer.parseInt(arr[2]), Integer.parseInt(arr[3])));
-            } else if (arr[0].equals("attack")) {
-                int attackerNum = Integer.parseInt(arr[1]);//攻击方当的随从编号
-                int defenderNum = Integer.parseInt(arr[2]);//被攻击放的随从编号
-                Summon attSummon = currentPlayer.get(attackerNum);//攻击方的随从
-                Summon deSummon = opponent.get(defenderNum);//被攻击方的随从
-                attSummon.health -= deSummon.attack;//生命值更新
-                deSummon.health -= attSummon.attack;//生命值更新
+            } else if ("attack".equals(arr[0])) {
+                int attackerNum = Integer.parseInt(arr[1]);
+                int defenderNum = Integer.parseInt(arr[2]);
+                Summon attSummon = currentPlayer.get(attackerNum);
+                Summon deSummon = opponent.get(defenderNum);
+                attSummon.health -= deSummon.attack;
+                deSummon.health -= attSummon.attack;
                 if (attSummon.health <= 0 ) {
                     currentPlayer.remove(attackerNum);
                 }
-                if (deSummon.health <= 0 && defenderNum!=0) {//如果被攻击方的生命值小于0，且被攻击放不是英雄，则删掉
+                if (deSummon.health <= 0 && defenderNum != 0) {
                     opponent.remove(defenderNum);
                 }
-            } else if (arr[0].equals("end")) {
+            } else if ("end".equals(arr[0])) {
                 if (currentPlayer.equals(list1)) {
                     currentPlayer = list2;
                     opponent = list1;
@@ -42,34 +45,40 @@ public class Main {
             }
         }
         if (list2.getFirst().health <= 0) {
-            System.out.println("1");
+            LOGGER.info("1");
         } else if (list1.getFirst().health <= 0) {
-            System.out.println("-1");
+            LOGGER.info("-1");
         } else {
-            System.out.println("0");
+            LOGGER.info("0");
         }
-        System.out.println(list1.getFirst().health);
-        System.out.print(list1.size() - 1 + " ");
+        LOGGER.info(String.valueOf(list1.getFirst().health));
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append(list1.size() - 1).append(" ");
         for (int i = 1; i < list1.size(); i++) {
-            System.out.print(list1.get(i).health + " ");
+            sb1.append(list1.get(i).health).append(" ");
         }
-        System.out.println();
-        System.out.println(list2.getFirst().health);
-        System.out.print(list2.size() - 1 + " ");
+        LOGGER.info(sb1.toString());
+        LOGGER.info(String.valueOf(list2.getFirst().health));
+        StringBuilder sb2 = new StringBuilder();
+        sb2.append(list2.size() - 1).append(" ");
         for (int i = 1; i < list2.size(); i++) {
-            System.out.print(list2.get(i).health + " ");
+            sb2.append(list2.get(i).health).append(" ");
         }
+        LOGGER.info(sb2.toString());
     }
 }
+
 class Summon {
     int attack;
     int health;
+
     public Summon(int attack, int health) {
-        super();
         this.attack = attack;
         this.health = health;
     }
+
+    @Override
     public String toString() {
-        return "attach:" + String.valueOf(attack) + " health:" + String.valueOf(health);
+        return "attack:" + attack + " health:" + health;
     }
 }
